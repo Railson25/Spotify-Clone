8c0bc5eac19bb57b98d051b039238aa9f8d1eacd
Railson25 <railsonsantiago123@gmail.com>
2023-08-25 12:06:56 -0300

    <fix>  stripe types

--- types.ts
+++ types.ts
@@ -40,7 +40,7 @@ export interface Subscription{
     status?: Stripe.Subscription.Status
     metadata?: Stripe.Metadata
     price_id?: string
-    quantity?: string
+    quantity?: number
     cancel_at_period_end?: boolean
     created: string
     current_period_start: string
@@ -50,5 +50,5 @@ export interface Subscription{
     canceled_at?: string
     trial_start?: string
     trial_end?: string
-    pricees?: Price
+    prices?: Price
 }
