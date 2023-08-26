632d45c44a05cad5f3ba47ba5ba9c4d6e2d9c605
Railson25 <railsonsantiago123@gmail.com>
2023-08-26 14:17:52 -0300

    <feat>UploadModal component

--- providers/modalProvider.tsx
+++ providers/modalProvider.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { AuthModal } from "@/components/AuthModal"
+import { UploadModal } from "@/components/UploadModal"
 
 export const ModalProvider = () => {
 
     return(
         <>
            <AuthModal />
+           <UploadModal />
         </>
     )
 }
