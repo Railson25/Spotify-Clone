81921895a785f2822b0672b3cd375527b9013978
Railson25 <railsonsantiago123@gmail.com>
2023-08-27 14:50:12 -0300

    <chore> adding next config to allow images domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains: [
+            "guyklziujspeosggnaoj.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
