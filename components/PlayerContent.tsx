75dd7fc2aeba09bdf4cf2711e63f7111a7ff082b
Railson25 <railsonsantiago123@gmail.com>
2023-08-30 15:42:11 -0300

    <feat>: functions to play music

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -8,7 +8,8 @@ import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai"
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2"
 import { Slider } from "./Slider"
 import { usePlayer } from "@/hooks/usePlayer"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import useSound from 'use-sound';
 
 interface PlayerContentProps{
     song: Song
@@ -54,6 +55,43 @@ export const PlayerContent  = ({song, songUrl}: PlayerContentProps) => {
         player.setId(previousSong)
     }
 
+    const [play, {pause, sound}] = useSound(
+        songUrl, 
+        {
+            volume: volume,
+            onplay: () => setIsPlaying(true),
+            onend: () => {
+                setIsPlaying(false),
+                onPlayNext()
+            },
+            onpause: () => setIsPlaying(false),
+            format: ['mp3']
+        }
+    )
+
+
+    useEffect(() => {
+        sound?.play()
+        return () => {
+            sound?.unload()
+        }
+    }, [sound])
+
+    const handlePlay = () => {
+        if(!isPlaying){
+            play()
+        }else {
+            pause()
+        }
+    }
+
+    const toggleMute = () => {
+        if(volume === 0){
+            setVolume(1)
+        }else {
+            setVolume(0)
+        }
+    }
 
     return(
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -65,7 +103,7 @@ export const PlayerContent  = ({song, songUrl}: PlayerContentProps) => {
             </div>
             <div className="flex md:hidden col-auto w-full justify-end items-center">
                 <div 
-                    onClick={() => {}}
+                    onClick={handlePlay}
                     className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
                 >
                     <Icon  size={30} className='text-black' />
@@ -78,7 +116,7 @@ export const PlayerContent  = ({song, songUrl}: PlayerContentProps) => {
                     className='text-neutral-400 cursor-pointer hover:text-white transition'
                 />
                 <div
-                   onClick={() => {}} 
+                   onClick={handlePlay} 
                    className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
                 >
                     <Icon size={30} className='text-black'  />
@@ -92,11 +130,14 @@ export const PlayerContent  = ({song, songUrl}: PlayerContentProps) => {
             <div className="hidden md:flex w-full justify-end pr-2">
                 <div className="flex items-center gap-x-2 w-[120px]">
                     <VolumeIcon 
-                        onClick={() => {}}
+                        onClick={toggleMute}
                         size={34}
                         className='cursor-pointer'
                     />
-                    <Slider />
+                    <Slider 
+                        value={volume}
+                        onChange={(value) => setVolume(value)}
+                    />
                 </div>
             </div>
         </div>
