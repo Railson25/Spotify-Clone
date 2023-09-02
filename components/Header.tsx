8cd778a25fb0e06c513b33b86c398ec81942b4ed
Railson25 <railsonsantiago123@gmail.com>
2023-09-02 16:42:10 -0300

    <feat>: close play component when logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,6 +11,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react"
 import { useUser } from "@/hooks/useUser"
 import { FaUserAlt } from "react-icons/fa"
 import { toast } from "react-hot-toast"
+import { usePlayer } from "@/hooks/usePlayer"
 
 interface HeaderProps{
     children: React.ReactNode
@@ -19,6 +20,7 @@ interface HeaderProps{
 
 export const Header = ({children, className}: HeaderProps) => {
 
+    const player = usePlayer()
     const router = useRouter()
     const authModal = useAuthModal()
     const supabaseClient = useSupabaseClient() 
@@ -28,6 +30,7 @@ export const Header = ({children, className}: HeaderProps) => {
     const handleLogout = async () => {
         const {error} = await supabaseClient.auth.signOut()
 
+        player.reset()
         router.refresh() 
 
         if(error){
