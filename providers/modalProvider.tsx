9c6637562d5e0147f8103c302358481247e7f0b0
Railson25 <railsonsantiago123@gmail.com>
2023-09-02 16:39:50 -0300

    <feat>: adding props to modal provider

--- providers/modalProvider.tsx
+++ providers/modalProvider.tsx
@@ -1,14 +1,21 @@
 "use client"
 
 import { AuthModal } from "@/components/AuthModal"
+import { SubscribeModal } from "@/components/SubscribeModal"
 import { UploadModal } from "@/components/UploadModal"
+import { ProductWithPrice } from "@/types"
 
-export const ModalProvider = () => {
+interface ModalProviderProps{
+    products: ProductWithPrice[]
+}
+
+export const ModalProvider = ({products}: ModalProviderProps) => {
 
     return(
         <>
            <AuthModal />
            <UploadModal />
+           <SubscribeModal products={products} />
         </>
     )
 }
